0256bbe8fa9f65643ad613826810eb41d0686006
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-05-29 17:30:56 +0800

    Started implementing basic game logic

--- plugins/FarkleBackend/farklebackend.cpp
+++ plugins/FarkleBackend/farklebackend.cpp
@@ -2,11 +2,34 @@
 
 #include "farklebackend.h"
 
+void FarkleBackend::startGame(int pCount, int turnLimit) {
+	if (!roll) {
+		roll = (Roll*)malloc(sizeof(Roll));
+	}
+	turns = turnLimit;
+	if (players) {
+		for (int i = 0; i < this->pCount; i++) {
+			freePlayer(players[i]);
+		}
+		free(players);
+	}
+	this->pCount = pCount;
+	players = (Player**)malloc(pCount * sizeof(Player*));
+	initRoll(roll);
+}
+
 void FarkleBackend::newRoll() {
+	newRoll(roll);
 }
 
 void FarkleBackend::confirmSelection() {
+	Selection* sel = (Selection*)malloc(sizeof(Selection));
+        if (constructSelection(roll, sel)) {
+        } else {
+                deselectRoll(_roll);
+        }
 }
 
 void FarkleBackend::bankPoints() {
+	bankPoints(players[currentPlayer]);
 }
--- plugins/FarkleBackend/farklebackend.h
+++ plugins/FarkleBackend/farklebackend.h
@@ -8,7 +8,13 @@
 class FarkleBackend: public QObject {
     Q_OBJECT
 
+    GameState state;
+    Roll* roll;
+    Player** players;
+    int turn, turns, pCount, currentPlayer;
+
 public:
+    Q_INVOKABLE void startGame(int, int);
     Q_INVOKABLE void newRoll();
     Q_INVOKABLE void confirmSelection();
     Q_INVOKABLE void bankPoints();
